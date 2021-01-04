1fa7f0de6adecdb094e690e9a78ef369ec75732a
unknown <webdev.hitesh@gmail.com>
2021-01-04 21:01:16 +0530

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{useState} from "react"
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Homepage from "./components/Homepage"
 import LandingPage from "./components/LandingPage"
 import './sass/App.css'
@@ -9,16 +8,6 @@ function App() {
     <div className="App">
       <LandingPage/>
       <Homepage/>
-     {/* <Router>
-      <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        {
-          width <= 500 ? <Route exact path="/home" component={Homepage}/> : ""
-        }
-        
-      </Switch>
-      </Router> 
-      {width >= 500 ? <Homepage/> : ""} */}
     </div>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect} from 'react'
 import {useInView} from "react-intersection-observer"
 import { motion,useAnimation} from "framer-motion";
 import {specialMenu,popularItems} from '../data'
